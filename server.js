d680ec7a84aa694c7595172874445fc951c7a7ea
nziza-dev-og <nzizadev@gmail.com>
2026-04-01 12:09:37 +0200

    updated auth middleware

--- server.js
+++ server.js
@@ -41,7 +41,32 @@ app.post('/user/login' , (req , res) =>{
       res.json({token});
 
     } );
-}); 
+});
+
+function auth(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token format invalid' });
+  }
+
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
 
  
 app.get('/user' , (req , res) =>{
